959d39f423dd31eb10d92e7e36706f6a96484fca
Salomanuel <salomanuel.aux+code@gmail.com>
2017-08-21 16:23:48 +0200

    populate the db with feeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+survey1 = Survey.create!(name: "food in the Star Wars galaxy")
+survey2 = Survey.create!(name: "Survey 2")
+
+3.times do |i|
+	survey1.questions.create!(content: "do you like #{Faker::Food.dish}?")
+end
+
+5.times do |i|
+	Participant.create(name: Faker::StarWars.character)
+end
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
